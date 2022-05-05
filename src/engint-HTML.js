6ebd7ebbc8b51a8041a401d9d4091b797fb619f0
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-05 11:28:08 -0500

    HTML generation complete

--- src/engint-HTML.js
+++ src/engint-HTML.js
@@ -1,26 +1,67 @@
 const fs = require('fs')
+const writeFile = require("../src/generate-site")
+// const generateManager = require('./manager-HTML')
+const totalInfo = []
 
-const generateEngineer = function(name, id, email, github) {
-    const engineerInformation = `
-<div>
-<h1>${name}</h1>
-<h1>${id}</h1>
-<h1>${email}</h1>
-<h1>${github}</h1>
-</div>
-`
-    console.log(engineerInformation)
+
+const generateManager = function(name, id, email, role, officeNumber) {
+    const managerInformation = `
+    <div class="container">
+    <h2>${name}</h2>
+    <h3>${role}</h3>
+    <h3>Employee ID: ${id}</h3>
+    <h3>Email Address: <a href="mailto:${email}">${email}</a></h3>
+    <h3>Office Number:${officeNumber}</h3>
+    </div>
+    `
+    totalInfo.push(managerInformation)
 }
-const generateIntern = function(name, id, email, school) {
+const generateEngineer = function(name, id, email, role, github) {
+    const engineerInformation = `
+    <div class="container">
+    <h2>${name}</h2>
+    <h3>${role}</h3>
+    <h3>Employee ID: ${id}</h3>
+    <h3>Email Address: <a href="mailto:${email}">${email}</a></h3>
+    <h3>GitHub: <a href="https://github.com/${github}">https://github.com/${github}</a></h3>
+    </div>
+    `
+    totalInfo.push(engineerInformation)
+}
+
+const generateIntern = function(name, id, email, role, school) {
     const internInformation = `
-<div>
-<h1>${name}</h1>
-<h1>${id}</h1>
-<h1>${email}</h1>
-<h1>${school}</h1>
-</div>
-`
-    console.log(internInformation)
+    <div class="container">
+    <h2>${name}</h2>
+    <h3>${role}</h3>
+    <h3>Employee ID: ${id}</h3>
+    <h3>Email Address: <a href="mailto:${email}">${email}</a></h3>
+    <h3>School: ${school}</h3>
+    </div>
+    `
+    totalInfo.push(internInformation)
+}
+
+const putItAllTogether = function(yaya) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <title>Portfolio Demo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="../src/styles.css">
+    </head>
+
+    <body>
+    <h1>Employee List</h1>
+    ${yaya}
+    </body>
+    `
 }
 
 const engintHTML = function(memberInfo) {
@@ -34,35 +75,19 @@ const engintHTML = function(memberInfo) {
             generateEngineer(name, id, email, role, github)
 
         }
+        else if(role === "Manager") {
+            officeNumber = element.officeNumber
+            generateManager(name, id, email, role, officeNumber)
+        }
         else if(role === "Intern") {
             school = element.school
             generateIntern(name, id, email, role, school)
         }
     })
+    console.log(totalInfo)
+    const totalInfoJoined = totalInfo.join(" ")
 
-
-
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie-edge">
-        <title>Portfolio Demo</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-    </head>
-
-    <body>
-        <header>
-
-        </header>
-    </body>
-
-    `
+    writeFile(putItAllTogether(totalInfoJoined))
 }
 
 
